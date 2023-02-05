b873011d80e815618d7021d5b9290593ed8adad3
mikhailone <vbiffff.nik@gmail.com>
2023-02-05 13:33:32 +0300

    all fix

--- js/tabs.js
+++ js/tabs.js
@@ -1,20 +1,14 @@
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+const tabsBtns = document.querySelectorAll('.tablinks');
+const tabs = document.querySelectorAll('.tabcontent');
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+for (let i = 0; i < tabsBtns.length; i++) {
+    tabsBtns[i].addEventListener('click', () => {
+        tabsBtns.forEach((item, i) => {
+            tabsBtns[i].classList.remove('tab-active');
+            tabs[i].setAttribute('hidden', 'hidden');
+        });
+        tabsBtns[i].classList.add('tab-active');
+        tabs[i].removeAttribute('hidden', 'hidden');
+    });
 }
